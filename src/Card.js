f3da668b42e2ee6e1e46d09ef0d27d7d6aaadace
charles <weichetsai@gmail.com>
2019-03-22 09:07:12 +0800

    dim the card

--- src/Card.js
+++ src/Card.js
@@ -5,23 +5,23 @@ class Card extends Component {
 
     render() {
         let cardStyle = {
-            width: "200px",
-            height: "300px",
-            marginBottom: "0px",
-            backgroundColor: "gray",
+            marginBottom: "-2px",
+            backgroundColor: "white",
             borderStyle: "solid",
-            borderWidth: "5px",
+            borderWidth: "3px",
             borderColor: "red"
           };
         let msg = ""
         if (this.props.state === "fail") {
             cardStyle['borderColor'] = "red";
-            msg = "install with error..."
+            msg = "install with error...";
+            cardStyle['filter'] = `brightness(60%)`;
         } else
             cardStyle['borderColor'] = "blue";
     return (
     <Tile>
-        <Tile style={cardStyle}>
+        <Tile style={cardStyle} >
+        <h1 style={{color:"red", width: "180px", height: "200px",}}>CARD</h1>
         </Tile>
         <div style={{fontSize:14, color:"red"}}>{msg}</div>
     </Tile>);
